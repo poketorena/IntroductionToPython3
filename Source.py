3f4c85073259fe495e6fa527e4d2325849a28100
poketorena <poketorena@users.noreply.github.com>
2018-11-18 01:00:03 +0900

    2進数、8進数、16進数の表し方、print()すると10進数で出力される

--- Source.py
+++ Source.py
@@ -1,8 +1,6 @@
-import math
-
-# 小数点以下第1位の四捨五入
-print(round(23.4))
-# 小数点以下第1位の切り上げ
-print(math.ceil(23.4))
-# 小数点以下第1位の切り捨て
-print(math.floor(23.4))
+# 2進数
+print(0b0101)
+# 8進数
+print(0o011)
+# 16進数
+print(0xFF)
