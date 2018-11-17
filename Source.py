b94f56456d209a88b3c1e95471ffa5b16fd0dd15
poketorena <poketorena@users.noreply.github.com>
2018-11-18 00:57:14 +0900

    小数点以下第1位の四捨五入、切り上げ、切り捨て

--- Source.py
+++ Source.py
@@ -1,3 +1,8 @@
-print(round(23.51))
-print(round(23.54 * 10) / 10)
-print(round(23.55 * 10) / 10)
+import math
+
+# 小数点以下第1位の四捨五入
+print(round(23.4))
+# 小数点以下第1位の切り上げ
+print(math.ceil(23.4))
+# 小数点以下第1位の切り捨て
+print(math.floor(23.4))
