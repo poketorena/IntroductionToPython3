6a79f8b50a886e9a8964493a68a25ea50b02aed0
poketorena <poketorena@users.noreply.github.com>
2019-04-18 20:37:31 +0900

    andとor

--- Source.py
+++ Source.py
@@ -1,4 +1,23 @@
 import math
 
-# 浮動小数点の余りの計算
-print(math.fmod(12.3, 1.1))
+# 両方ともbool型でないときはorは左、andは右が返ってくる
+a = 1 or 2
+b = 3 and 4
+c = True or 5
+d = False or 6
+e = True and 7
+f = False and 8
+g = 9 or True
+h = 10 or False
+i = 11 and True
+j = 12 and False
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
