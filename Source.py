545c6b192fb5657d18b770be561e22a72b014b40
poketorena <poketorena@users.noreply.github.com>
2019-04-18 20:58:05 +0900

    if else を1行で書く
    この書き方は分かりにくいので使わないようにしよう

--- Source.py
+++ Source.py
@@ -1,7 +1,8 @@
 from random import randint
-a = randint(0, 10)
-# 判定
-if 5 <= a <= 8:
-    print(f"{a}:合格")
-else:
-    print(f"{a}:不合格")
+a = randint(0, 100)
+b = randint(0, 100)
+
+bigger = a if a > b else b
+
+text = f"{a}と{b}では{bigger}が大きい"
+print(text)
