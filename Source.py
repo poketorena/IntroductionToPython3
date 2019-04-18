71d32f7e9d570ae5fab9cc737a5e966b178ffaab
poketorena <poketorena@users.noreply.github.com>
2019-04-18 20:50:03 +0900

    and、filter、lambdaで不要な要素を除去したリストを作る

--- Source.py
+++ Source.py
@@ -1,23 +1,10 @@
 import math
 
-# 両方ともbool型でないときはorは左、andは右が返ってくる
-a = 1 or 2
-b = 3 and 4
-c = True or 5
-d = False or 6
-e = True and 7
-f = False and 8
-g = 9 or True
-h = 10 or False
-i = 11 and True
-j = 12 and False
-print(a)
-print(b)
-print(c)
-print(d)
-print(e)
-print(f)
-print(g)
-print(h)
-print(i)
-print(j)
+# and、filter、lambdaで不要な要素を除去したリストを作る
+list_1 = [' ',' ','/','/','', '', '1', '2', '3']
+
+print(list_1)
+
+list_1 = list(filter(lambda x: x != ' ' and x != '/' and x != '', list_1))
+
+print(list_1)
