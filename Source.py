f00b12fa7018c9af83b563b5d38850e0bd7246f7
poketorena <poketorena@users.noreply.github.com>
2019-04-27 00:53:08 +0900

    文字列をリストに分割する

--- Source.py
+++ Source.py
@@ -1,8 +1,5 @@
-evenlist = list(range(0, 10, 2))  # 偶数のリスト
-print(evenlist)
+list_happy = list("happy")
+print(list_happy)
 
-oddlist = list(range(1, 10, 2))  # 奇数のリスト
-print(oddlist)
-
-list_x3 = list(range(0, 20, 3))  # 3の倍数のリスト
-print(list_x3)
+week = list("日月火水木金土")
+print(week)
