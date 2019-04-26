f7baeca65334c62b3dc816cbc6ab055cb26b2a28
poketorena <poketorena@users.noreply.github.com>
2019-04-27 00:51:48 +0900

    range()からリストを作る

--- Source.py
+++ Source.py
@@ -1,2 +1,8 @@
-thelist = list(range(-5, 6))
-print(thelist)
+evenlist = list(range(0, 10, 2))  # 偶数のリスト
+print(evenlist)
+
+oddlist = list(range(1, 10, 2))  # 奇数のリスト
+print(oddlist)
+
+list_x3 = list(range(0, 20, 3))  # 3の倍数のリスト
+print(list_x3)
