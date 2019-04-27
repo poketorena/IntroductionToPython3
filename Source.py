edecb57f1397ee239e828d184657e04ce1d68b32
poketorena <poketorena@users.noreply.github.com>
2019-04-27 19:03:46 +0900

    リストをスライスする

--- Source.py
+++ Source.py
@@ -1,3 +1,9 @@
-members = ["Tom", "Jerry", "Spike"]
-name = " and ".join(members)
-print(name)
+colors = ["blue", "red", "green", "yellow", "pink", "black", "white"]
+print(colors[:])
+print(colors[3:])
+print(colors[:3])
+print(colors[3:6])
+print(colors[-1:])
+print(colors[-2:])
+print(colors[-2:-1])
+print(colors[:-1])
