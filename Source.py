6c0fe88b727c6b306acb9cba74403720a6b797fb
poketorena <poketorena@users.noreply.github.com>
2019-04-27 18:45:57 +0900

    削除したい値をリストからすべて削除する

--- Source.py
+++ Source.py
@@ -1,6 +1,6 @@
 colors = ["blue", "red", "yellow", "red", "green"]
 print("削除前", colors)
 target = "red"
-if target in colors:
+while target in colors:
     colors.remove(target)
 print("削除後", colors)
