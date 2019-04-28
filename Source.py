1051a62bf843bcba6000d7bdf42eda5e61f02d5c
poketorena <poketorena@users.noreply.github.com>
2019-04-29 00:04:03 +0900

    タプルの値とオブジェクトを比較する

--- Source.py
+++ Source.py
@@ -1,4 +1,12 @@
-data = (56, 45, 83, 67, 59, 41, 77)
-print(sum(data))
-print(max(data))
-print(min(data))
+a = (1, 2, 3)
+b = a
+c = (1, 2, 3)
+
+print(a == b)
+print(a is b)
+print(a == c)
+print(a is c)  # Falseのはずだけど同じidが割り当てられることもある？？？
+
+print(id(a))
+print(id(b))
+print(id(c))
