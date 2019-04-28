afecadba9824aa35762e023c68e54f6911ec1550
poketorena <poketorena@users.noreply.github.com>
2019-04-28 22:03:38 +0900

    複数のリストの要素同士を連結した新しいリストを作る

--- Source.py
+++ Source.py
@@ -1,3 +1,8 @@
-names = ["鈴木", "田中", "栗林", "山岡"]
-for i, who in enumerate(names, 1):
-    print(f"{i}:{who}さん")
+name1 = ["鈴木", "田中", "赤尾", "佐々木", "高田"]
+name2 = ["星奈", "優美", "恵子", "薫花", "幸恵"]
+longname = []
+
+# name1とname2を連結したリストを作る
+for n1, n2 in zip(name1, name2):
+    longname.append(n1 + n2)
+print(longname)
