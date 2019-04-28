afdb289f0a2cd6e3e4929d81eea297d43c0211f7
poketorena <poketorena@users.noreply.github.com>
2019-04-28 19:18:52 +0900

    スライスの返礼地は元のリストを部分的にディープコピーしたもの？
    もとのリストを変更してもスライス後のリストに影響しない！

--- Source.py
+++ Source.py
@@ -3,5 +3,14 @@ n = 3
 data1 = data[:n]  # 最初からn-1まで
 data2 = data[n:]  # nから最後まで
 
+print(data)
 print(data1)
 print(data2)
+
+data[0] = 999
+
+# スライスの返礼地は元のリストを部分的にディープコピーしたもの？
+# もとのリストを変更してもスライス後のリストに影響しない！
+print()
+print(data)
+print(data1)
