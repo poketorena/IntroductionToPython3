4a6e8d0d8c3a982148219e210a7f1a6c9b0bd7d0
poketorena <poketorena@users.noreply.github.com>
2019-04-28 22:32:48 +0900

    タプルを作る

--- Source.py
+++ Source.py
@@ -1,5 +1,9 @@
-import secrets
+a = (1, 2)
+b = ("py", 3.6)
+c = (89, 56, 75)
+d = ((10, 20), (30, 40))
 
-fruits = ["apple", "orange", "banana", "peach"]
-dessert = secrets.choice(fruits)
-print(dessert)
+print(a)
+print(b)
+print(c)
+print(d)
