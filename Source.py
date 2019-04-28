73ccaa17b88781dc41c12f730d22f40497a096db
poketorena <poketorena@users.noreply.github.com>
2019-04-29 00:11:08 +0900

    zip(name1, name2)の結果を確かめる

--- Source.py
+++ Source.py
@@ -1,6 +1,4 @@
-x = [1, 2, 3]
-y = [4, 5, 6]
-z = [7, 8, 9]
-zip_obj = zip(x, y, z)
-xyz = list(zip_obj)
-print(xyz)
+name1 = ["鈴木", "田中", "赤尾", "佐々木", "高田"]
+name2 = ["星奈", "優美", "恵子", "薫花", "幸恵"]
+zip_obj = zip(name1, name2)
+print(list(zip_obj))
