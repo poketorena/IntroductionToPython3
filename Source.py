f92f9fae8723fa7789e7258401c940953091ac72
poketorena <poketorena@users.noreply.github.com>
2019-04-28 21:58:04 +0900

    ループカウンタを付けて表示する

--- Source.py
+++ Source.py
@@ -1,6 +1,3 @@
-words = ["peach", "ver3", "Python3", "Pokemon", "ver2"]
-print(words)
-words.sort()
-print(words)
-words.sort(key=str.lower)
-print(words)
+names = ["鈴木", "田中", "栗林", "山岡"]
+for i, who in enumerate(names, 1):
+    print(f"{i}:{who}さん")
