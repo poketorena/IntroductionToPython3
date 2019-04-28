54a3c69c12e8f7a88ab38e11be64efe95520ffac
poketorena <poketorena@users.noreply.github.com>
2019-04-28 20:03:25 +0900

    list_workの値を変更してもlist_motherは元のまま

--- Source.py
+++ Source.py
@@ -3,3 +3,9 @@ list_work = list_mother.copy()  # リストを複製する（浅いコピー）
 print(list_mother)
 print(list_work)
 print(list_work is list_mother)  # 値は同じだけど別のオブジェクト
+
+list_work[0] = 99
+
+print()
+print(list_mother)
+print(list_work)
