b5fc6a63202fdc1f1de9ba377b6f1376b630a8bf
poketorena <poketorena@users.noreply.github.com>
2019-04-28 19:30:13 +0900

    先に範囲でスライスする場合と先にステップでスライスする場合の違い

--- Source.py
+++ Source.py
@@ -1,4 +1,4 @@
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-print(letters)
-print(letters[::2])
-print(letters[1::2])
+print(letters[1:5][::2])  # 先に範囲でスライスする
+print(letters[::2][1:5])  # 先にステップでスライスする
+print(letters[1:5:2])  # 範囲とステップを同時に指定する
