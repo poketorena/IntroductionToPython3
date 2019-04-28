5aebfee756bf26834f6021d46f5758d797d0e100
poketorena <poketorena@users.noreply.github.com>
2019-04-28 19:25:42 +0900

    要素を1個飛ばしで取り出す

--- Source.py
+++ Source.py
@@ -1,16 +1,4 @@
-data = [10, 21, 35, 49, 51, 60, 77, 81, 92, 100]
-n = 3
-data1 = data[:n]  # 最初からn-1まで
-data2 = data[n:]  # nから最後まで
-
-print(data)
-print(data1)
-print(data2)
-
-data[0] = 999
-
-# スライスの返礼地は元のリストを部分的にディープコピーしたもの？
-# もとのリストを変更してもスライス後のリストに影響しない！
-print()
-print(data)
-print(data1)
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters)
+print(letters[::2])
+print(letters[1::2])
