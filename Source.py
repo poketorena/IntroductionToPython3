74fb8454fd90c32433ce10b2173c366194c3df87
poketorena <poketorena@users.noreply.github.com>
2019-04-28 22:10:16 +0900

    数値以外の値を取り除く

--- Source.py
+++ Source.py
@@ -1,3 +1,3 @@
-numbers = [2.1, 0.2, 0.3, 1.4, 3.1, 0.3, 1.6]
-result = [num for num in numbers if 1 <= num < 2]
-print(result)
+numbers = [2.1, 4, "", 2.2, "1", 3]
+numbers = [num for num in numbers if isinstance(num, (int, float))]
+print(numbers)
