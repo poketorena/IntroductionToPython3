1d61a9438faa88a003eb286dc7a251ac30c39241
poketorena <poketorena@users.noreply.github.com>
2019-05-04 21:04:02 +0900

    range()を使ってセットを作る

--- Source.py
+++ Source.py
@@ -1,2 +1,4 @@
-color_set = {"blue", "pink", "orange", "white"}
-print("red" not in color_set)
+num2set = set(range(0, 20, 2))  # 0～19の2の倍数のセット
+num3set = set(range(0, 20, 3))  # 0～19の3の倍数のセット
+print(num2set)
+print(num3set)
