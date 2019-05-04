f87e675481b71e7400f4d14b167f87c978f0279b
poketorena <poketorena@users.noreply.github.com>
2019-05-04 21:15:41 +0900

    リストからセットを作る（重複した要素は取り除かれる）

--- Source.py
+++ Source.py
@@ -1,10 +1,4 @@
-empty_set1 = {}
-empty_set2 = set()
-print(empty_set1)
-print(empty_set2)
-
-print(empty_set1 == empty_set2)
-print(empty_set1 is empty_set2)
-
-print(type(set()))
-print(type({}))
+data = [101, 103, 103, 115, 167, 167, 189]
+dataset = set(data)
+print(data)
+print(dataset)
