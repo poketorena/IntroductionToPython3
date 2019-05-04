eb97c9f713a35084d215431e0d804e90a74f0e20
poketorena <poketorena@users.noreply.github.com>
2019-05-04 21:19:14 +0900

    文字列からセットを作る

--- Source.py
+++ Source.py
@@ -1,11 +1,2 @@
-data = [101, 103, 103, 115, 167, 167, 189]
-dataset = set(data)
-
-print(data)
-print(dataset)
-
-data = list(dataset)
-
-print(data)
-data.sort()
-print(data)
+happyset = set("happy")
+print(happyset)
