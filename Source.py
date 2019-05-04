04b729080e9b99639c4711451eb650b3c264212d
poketorena <poketorena@users.noreply.github.com>
2019-05-04 20:57:25 +0900

    セットの要素の個数

--- Source.py
+++ Source.py
@@ -1,11 +1,2 @@
-set1 = {
-    11, 22, 33, 44, 55,  # コメント
-    111, 222
-}
-
-set2 = {
-    33, 66, 99  # コメント
-}
-
-print(f"set1: {set1}")
-print(f"set2: {set2}")
+color_set = {"blue", "pink", "orange", "white", "black"}
+print(len(color_set))
