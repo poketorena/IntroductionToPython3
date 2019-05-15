211aea09d6a78a5866720157387be4fdd9e8be37
poketorena <poketorena@users.noreply.github.com>
2019-05-16 00:10:22 +0900

    和集合：すべてのセットの要素を合わせたセットを作る

--- Source.py
+++ Source.py
@@ -1,3 +1,4 @@
-numbers = [-1.3, 1.2, -1.2, 1.1, 1.5, -1.1, 1.2, 1.1, 1.4]
-num_set = {num for num in numbers if num > 0}
-print(num_set)
+a = {"りんご", "みかん", "桃", "いちご"}
+b = {"いちご", "スイカ", "みかん", "バナナ"}
+c = a | b  # 和集合を求める
+print(c)
