00bcce80b4c4fd608c1d1bf9ce79f9f388ab60b3
poketorena <poketorena@users.noreply.github.com>
2019-05-17 18:51:29 +0900

    セットの値を差集合で置き換える（イテラブルの場合）

--- Source.py
+++ Source.py
@@ -1,5 +1,5 @@
 data = {"red", "blue", "green", "yellow"}
-data2 = {"blue", "black", "yellow"}
+data2 = ["blue", "black", "yellow"]  # メソッドなのでいてラブルもOK!
 
 data.difference_update(data2)  # 差集合で更新する
 
