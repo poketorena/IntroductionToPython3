e8d8bd0158cbf97fb0504b86ddd8a3d1cad1649c
poketorena <poketorena@users.noreply.github.com>
2019-05-17 18:47:05 +0900

    セットの値を積集合で置き換える（引数をイテラブルにした場合）

--- Source.py
+++ Source.py
@@ -1,5 +1,5 @@
 data = {"red", "blue", "green", "yellow"}
-data2 = {"blue", "black", "yellow"}
+data2 = ["blue", "black", "yellow"]  # メソッドなので引数にイテラブルが使える
 
 data.intersection_update(data2)  # 積集合で更新する
 
