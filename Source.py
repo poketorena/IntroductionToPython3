7c01680562318e2718da805d200ddd831b4dfcd7
poketorena <poketorena@users.noreply.github.com>
2019-05-17 18:42:15 +0900

    |=演算子を使って和集合の代入を書いた場合（update()と同じ作用）

--- Source.py
+++ Source.py
@@ -1,5 +1,8 @@
 data = {"red", "blue"}
 data2 = {"blue", "yellow"}
-data3 = ["blue", "green"]  # メソッドなので引数はイテラブルならOK!
-data.update(data2, data3)  # 和集合で更新する
+data3 = {"blue", "green"}
+
+data |= data2  # 和集合で置き換える
+data |= data3  # 和集合で置き換える
+
 print(data)
