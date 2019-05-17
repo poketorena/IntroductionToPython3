7729a65214545eeb0140aa5647229c9ba9f37ba3
poketorena <poketorena@users.noreply.github.com>
2019-05-17 19:20:05 +0900

    aセットがbセットのスーパーセット（上位集合）かどうかを判定する

--- Source.py
+++ Source.py
@@ -1,7 +1,7 @@
-a = {"blue", "red"}
-b = {"blue", "green", "red", "pink", "white"}
-c = ["blue", "green", "red", "pink", "white"]
-print(a.issubset(b))
-print(a.issubset(c))  # メソッドは引数にイテラブルを取れる！
-print(a <= b)
+a = {1999, 2011, 2013, 2014, 2016, 2017}
+b = {2011, 2013, 2014}
+c = [2011, 2013, 2014]
+print(a.issuperset(b))
+print(a.issuperset(c))  # メソッドは引数にイテラブルを取れる！
+print(a >= b)
 # print(a <= c)
