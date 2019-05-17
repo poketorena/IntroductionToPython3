1376d31750b832d010c9c6222aaa387de76b908b
poketorena <poketorena@users.noreply.github.com>
2019-05-17 19:06:15 +0900

    2つのセットの要素が一致しないときTrue

--- Source.py
+++ Source.py
@@ -1,6 +1,6 @@
 a = {1, 2, 3}
 b = {3, 2, 1}
 c = {1, 2, 3, 4}
-print(a == b)  # aとbの要素が一致しているのでTrue
+print(a != b)  # aとbの要素が一致しているのでFalse
 print(a is b)  # 同一インスタンスでないのでFalse
-print(a == c)  # cはaの要素をすべて含んでいるが同一の要素のみを持っているわけではないのでFalseになる
+print(a != c)  # aとcは違う要素を持っているのでTrue
