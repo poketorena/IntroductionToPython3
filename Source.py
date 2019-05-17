c6d6f119419665f256e50752ffb2f8a04c53143d
poketorena <poketorena@users.noreply.github.com>
2019-05-17 18:48:52 +0900

    &=演算子を使って積集合の代入を書いた場合

--- Source.py
+++ Source.py
@@ -1,6 +1,6 @@
 data = {"red", "blue", "green", "yellow"}
-data2 = ["blue", "black", "yellow"]  # メソッドなので引数にイテラブルが使える
+data2 = {"blue", "black", "yellow"}
 
-data.intersection_update(data2)  # 積集合で更新する
+data &= data2  # 積集合で置き換える
 
 print(data)
