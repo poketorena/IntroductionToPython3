1e150c517dedaed77e8d74f9633aad866a8b6099
poketorena <poketorena@users.noreply.github.com>
2019-05-20 23:15:04 +0900

    タプルがキーになっている辞書

--- Source.py
+++ Source.py
@@ -1,11 +1,3 @@
-stock = {"S": 7, "M": 12, "L": 3}
-result = {"t1": True, "t2": False, "t3": True}
-point = {10: 5.37, 20: 5.56, 30: 5.05, 40: 5.16}
-
-print(stock)
-print(result)
-print(point)
-
-print(type(stock))
-print(type(result))
-print(type(point))
+d = {(2011, "ab"): 10, (2011, "ax"): 12.5, (2013, "bw"): 16}
+print(d)
+print(type(d))
