bcb1eafd36b23737a639abb42292f2a975073749
poketorena <poketorena@users.noreply.github.com>
2019-05-20 23:48:27 +0900

    [キー, 値]のタプルから辞書を作る

--- Source.py
+++ Source.py
@@ -1,15 +1,3 @@
-keys = ["yellow", "blue", "green"]
-values = [3, 6, 5]
-
-zip_object = zip(keys, values)
-
-data_dict = dict(zip_object)
-
-# ここでzip_objectの2回目の評価を行っている！！！
-data_list = list(zip_object)
-
-# 普通に出力される
-print(data_dict)
-
-# zipなどで作られるイテレータは2回目以降の評価では何も返さない！！！！！
-print(data_list)
+data = dict((["yellow", 3], ["blue", 6], ["green", 5]))
+print(data)
+print(type(data))
