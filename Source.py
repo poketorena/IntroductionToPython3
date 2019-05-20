024149f692ac41f40c74a93efa936c9d4683fd05
poketorena <poketorena@users.noreply.github.com>
2019-05-20 23:59:05 +0900

    キーが存在しないときに要素を追加する

--- Source.py
+++ Source.py
@@ -1,5 +1,8 @@
 data = {"yellow": 3, "blue": 6, "green": 5}
 print(data)
-data["blue"] = 10
-data["white"] = 7
+result1 = data.setdefault("blue", 10)  # "blue"キーがあるので何も変更しない
+print(result1)
+print(data)
+result2 = data.setdefault("white", 10)  # "white"キーはないので要素を追加する
+print(result2)
 print(data)
