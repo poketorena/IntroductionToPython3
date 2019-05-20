299143f9f59bc1e22010e5dd4766343ca08f91fc
poketorena <poketorena@users.noreply.github.com>
2019-05-20 23:43:28 +0900

    キーのkeysリストと値のvaluesリストから辞書を作る

--- Source.py
+++ Source.py
@@ -1,3 +1,8 @@
-data = dict([("yellow", 3), ("blue", 6), ("green", 5)])
-print(data)
-print(type(data))
+keys = ["yellow", "blue", "green"]
+values = [3, 6, 5]
+
+zip_object = zip(keys, values)
+data_dict = dict(zip_object)
+# data_list = list(zip_object)
+print(data_dict)
+# print(data_list)
