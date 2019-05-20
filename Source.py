e94eb30e75ecaceb841484c94766b3ab08f3dc08
poketorena <poketorena@users.noreply.github.com>
2019-05-20 23:13:35 +0900

    辞書の例

--- Source.py
+++ Source.py
@@ -1,7 +1,11 @@
-a = {1999, 2011, 2013, 2014, 2016, 2017}
-b = {2011, 2013, 2014}
-c = [2011, 2013, 2014]
-print(a.issuperset(b))
-print(a.issuperset(c))  # メソッドは引数にイテラブルを取れる！
-print(a >= b)
-# print(a <= c)
+stock = {"S": 7, "M": 12, "L": 3}
+result = {"t1": True, "t2": False, "t3": True}
+point = {10: 5.37, 20: 5.56, 30: 5.05, 40: 5.16}
+
+print(stock)
+print(result)
+print(point)
+
+print(type(stock))
+print(type(result))
+print(type(point))
