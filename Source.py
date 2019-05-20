2580a33c8a0c1bf9b9d66b2139ad738fe337d31f
poketorena <poketorena@users.noreply.github.com>
2019-05-21 00:00:12 +0900

    空の辞書を作る

--- Source.py
+++ Source.py
@@ -1,8 +1,6 @@
-data = {"yellow": 3, "blue": 6, "green": 5}
-print(data)
-result1 = data.setdefault("blue", 10)  # "blue"キーがあるので何も変更しない
-print(result1)
-print(data)
-result2 = data.setdefault("white", 10)  # "white"キーはないので要素を追加する
-print(result2)
-print(data)
+d1 = {}
+d2 = dict()
+print(d1)
+print(d2)
+print(type(d1))
+print(type(d2))
