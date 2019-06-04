84a36b8902cd3ac26b967eae5f7cdeec02a6a97f
poketorena <poketorena@users.noreply.github.com>
2019-06-04 23:19:37 +0900

    ローカル変数は関数の外側から見れない！

--- Source.py
+++ Source.py
@@ -1,11 +1,10 @@
-def calc(num):
-    unit_price = 180  # 単価
-    try:
-        num = float(num)  # 数値に変換する
-        return num * unit_price
-    except:
-        return None  # 変換がエラーになったらNoneを返す
+def calc():
+    v = 2
+    "ローカル変数"
+    ans = 3 * v
+    print(ans)
 
-result = calc(2)
 
-print(result)
+# calc()を実行する
+calc()
+print(v)
