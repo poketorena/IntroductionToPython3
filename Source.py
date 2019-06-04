faafb482a8a26a1ed56cf42ed1ac2473c7dfe509
poketorena <poketorena@users.noreply.github.com>
2019-06-04 19:43:40 +0900

    KeyErrorを例外処理で回避する

--- Source.py
+++ Source.py
@@ -1,4 +1,10 @@
-# 辞書から値を取り出すから再開する
+city = input("調べる地区名：")
 members = {"東京": 21, "大阪": 16, "福岡": 11}
-print(members)
-print(members["沖縄"])
+# 例外処理に組み込む
+try:
+    value=members[city]
+    print(f"{city}の値は{value}です。")
+except KeyError:
+    print(f"{city}のデータはありません。")
+except Exception as error:
+    print(error)
