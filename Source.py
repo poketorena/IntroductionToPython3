4c81829372455123e572c0e9fdaec475c44a87a1
poketorena <poketorena@users.noreply.github.com>
2019-06-04 20:57:06 +0900

    すべてのキーの1文字目を大文字に変換したリストを作る

--- Source.py
+++ Source.py
@@ -1,9 +1,3 @@
 fruit = {"apple": 7, "orange": 5, "mango": 3, "peach": 6}
-print(type(fruit.keys()))
-print(fruit.keys())
-# list型のコンストラクタに渡してdict_keys型からlist型に変換する
-keys1 = list(fruit.keys())
-# これだとダメ
-keys2 = [fruit.keys()]
-print(keys1)
-print(keys2)
+keys = [key.capitalize() for key in fruit.keys()]
+print(keys)
