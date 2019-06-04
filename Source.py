6049a57c11db8bc0d05bdbb3108d02fd71cb49dd
poketorena <poketorena@users.noreply.github.com>
2019-06-04 23:54:05 +0900

    キーワード引数を利用して特定の引数だけ指定する

--- Source.py
+++ Source.py
@@ -1,6 +1,14 @@
-def price(adult, child):
-    return (adult * 1200) + (child * 500)
+def calc(size="M", num=6):
+    unit_price = {"S": 120, "M": 150, "L": 180}
+    price = unit_price[size] * num
+    return (size, num, price)
 
 
-print(price(adult=1, child=2))
-print(price(child=2, adult=1))
+a = calc()
+print(f"{a[0]}サイズ、{a[1]}個、{a[2]}円")
+
+b = calc("L")
+print(f"{b[0]}サイズ、{b[1]}個、{b[2]}円")
+
+c = calc(num=1)
+print(f"{c[0]}サイズ、{c[1]}個、{c[2]}円")
