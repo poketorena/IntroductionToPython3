3d3c48eea42b6fb9b75dc43096c3de38d8df0532
poketorena <poketorena@users.noreply.github.com>
2019-06-05 17:44:28 +0900

    最初の2個は必須の引数、残りはオプションの引数

--- Source.py
+++ Source.py
@@ -1,7 +1,14 @@
-def fruit(*args):
-    print(args)
+def route(start, end, *args):
+    # 引数からルートのリストを作る
+    route_list = [start]  # スタート地点
+    route_list += list(args)  # 経由地点
+    route_list += [end]  # ゴール地点
+    # リストの要素を→で連結した文字列にする
+    route_str = "→".join(route_list)
+    print(route_str)
 
 
-fruit()
-
-fruit("リンゴ", "みかん", "いちご", "バナナ")
+# routeを試す
+start = "東京"
+end = "宮崎"
+route(start, end, "神戸", "長崎", "熊本", "こなみ")
