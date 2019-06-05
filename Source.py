61ad76bd6ce8115649f12ec43fcc8bfc53af7450
poketorena <poketorena@users.noreply.github.com>
2019-06-05 19:00:44 +0900

    モジュールの使い方/作り方

--- Source.py
+++
@@ -1,9 +0,0 @@
-import exchange  # exchangeモジュールを読み込む
-
-yen = 25000
-rate = 114.22  # 円/ドル（中間値）
-charge = 1.0  # 為替手数料
-dollar = exchange.yen2dollar(yen, rate, charge)
-# 四捨五入もどき
-print(f"{dollar:,.2f}ドル")
-# print(f"{dollar}ドル")
