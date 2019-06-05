232e364aa254efc0f5ef874f2957e07d2a6b1e3d
poketorena <poketorena@users.noreply.github.com>
2019-06-05 19:51:48 +0900

    docstringの練習（pep484の型ヒントとGoogleスタイルのdocstringを使って関数の説明を書いていこう！）

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
+import cheer
 
-print(os.getcwd())  # カレントディレクトリを確認する
-
-os.chdir("./hoge")  # 現在のディレクトリにあるhogeフォルダに移動する
-
-print(os.getcwd())  # カレントディレクトリを確認する
+cheer.cheer()  # 引数を省略した場合
+cheer.cheer("五十嵐")  # 引数を入れた場合
