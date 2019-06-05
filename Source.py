54e2f89b334936e51cd3ba80426c671b546e62da
poketorena <poketorena@users.noreply.github.com>
2019-06-05 17:38:44 +0900

    可変長の引数*argsを試す

--- Source.py
+++ Source.py
@@ -1,14 +1,7 @@
-def calc(size="M", num=6):
-    unit_price = {"S": 120, "M": 150, "L": 180}
-    price = unit_price[size] * num
-    return (size, num, price)
+def fruit(*args):
+    print(args)
 
 
-a = calc()
-print(f"{a[0]}サイズ、{a[1]}個、{a[2]}円")
+fruit()
 
-b = calc("L")
-print(f"{b[0]}サイズ、{b[1]}個、{b[2]}円")
-
-c = calc(num=1)
-print(f"{c[0]}サイズ、{c[1]}個、{c[2]}円")
+fruit("リンゴ", "みかん", "いちご", "バナナ")
