84f89b9f93cf03abb838399f13cdc12220b5d17d
poketorena <poketorena@users.noreply.github.com>
2019-06-05 17:52:24 +0900

    キーワード引数を辞書で受け取る

--- Source.py
+++ Source.py
@@ -1,15 +1,5 @@
-def route(start, end, *args):
-    # 引数からルートのリストを作る
-    route_list = [start]  # スタート地点
-    route_list += list(args)  # 経由地点
-    route_list += [end]  # ゴール地点
-    # リストの要素を→で連結した文字列にする
-    route_str = "→".join(route_list)
-    print(route_str)
+def fruit(**kwargs):
+    print(kwargs)
 
 
-# routeを試す
-start = "東京"
-end = "宮崎"
-# route(start, end, "神戸", "長崎", "熊本", "こなみ")
-route(start, end)
+fruit(apple=2, orange=3, banana=1)
