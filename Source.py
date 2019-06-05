0b9dc355368b7d3ed517363d5f13e967acd34381
poketorena <poketorena@users.noreply.github.com>
2019-06-05 18:06:14 +0900

    コマンドライン引数で渡された数値の合計、平均を求める

--- Source.py
+++ Source.py
@@ -1,3 +1,15 @@
 import sys
 
-print(sys.argv)
+
+def calc(nums):
+    total = sum(nums)
+    print(f"合計：{total}")
+    if nums:  # numsが空ではないとき
+        ave = total / len(nums)
+        print(f"平均：{ave}")
+
+
+# コマンドライン引数で関数を実行する
+args = sys.argv[1:]  # 先頭のファイル名を除いたリスト
+nums = [float(num) for num in args]  # 数値のリストにする
+calc(nums)
