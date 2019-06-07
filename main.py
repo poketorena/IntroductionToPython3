b75a1567aca16c346428a9d3de2f7cf7c55e861f
poketorena <poketorena@users.noreply.github.com>
2019-06-07 15:14:09 +0900

    hello()を変数msgに代入して実行する

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import cheer
+def hello():
+    print("ハロー")
 
-cheer.cheer()  # 引数を省略した場合
-cheer.cheer("五十嵐")  # 引数を入れた場合
+
+msg = hello  # 関数を代入する
+
+print(hello)
+print(msg)
+
+msg()  # 変数msgに入っている関数を実行する
