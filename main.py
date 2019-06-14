56e901bb399cea6a2965e02bc166c3d5d895ceff
poketorena <poketorena@users.noreply.github.com>
2019-06-14 18:52:19 +0900

    sort()の比較関数をラムダ式で書く

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def size(item):
-    sizelist = ["XS", "S", "M", "L"]  # この順に並び替える
-    pos = sizelist.index(item)  # itemのインデックス番号を値として返す
-    return pos
-
+sizelist = ["XS", "S", "M", "L"]  # この順に並び替える
 
 # 並び替えるリスト
 data = ["S", "M", "XS", "L", "M", "M", "XS", "S", "M", "L", "M"]
-data.sort(key=size)
+data.sort(key=lambda item: sizelist.index(item))
 print(data)
