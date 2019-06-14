4aa0f0b181babc989a99d7adb2ef5b87d515b939
poketorena <poketorena@users.noreply.github.com>
2019-06-14 20:11:52 +0900

    イテラブルの型を表示する

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-nums = [2, -3, 9, 1, -2, -4, 5]
-num2 = [num for num in nums if num > 0]
-print(num2)
+colors = ["red", "blue", "green", "yellow"]
+colors_iter = iter(colors)
+type_dayo = type(colors_iter)
+
+print(type_dayo)
