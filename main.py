5bfe479c78557eedac576e418c2ae934389955a0
poketorena <poketorena@users.noreply.github.com>
2019-06-15 17:42:58 +0900

    非公開のインスタンス__nameをもつPersonクラス

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-class Person():
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+from person import Person
 
-
-class Player(Person):
-    def __init__(self, name, age, number, position):
-        super().__init__(name, age)
-        self.number = number
-        self.position = position
-
-
-player1 = Player("青木", 16, 10, "MF")
-
-print(player1.name)
-print(player1.age)
-print(player1.number)
-print(player1.position)
+man = Person("宇佐美")
+man.who()
+print(man.__name)
