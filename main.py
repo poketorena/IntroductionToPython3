19ec60ef26eb5e2b9f2bdc85ef5436efe8ce9b77
poketorena <poketorena@users.noreply.github.com>
2019-06-15 16:53:47 +0900

    Bクラスのインスタンスを作って継承したメソッドを試す

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-class Simple:
-    pass
+class A:  # Aクラス
+    def hello(self):
+        print("ハロー")
 
 
-obj1 = Simple()
-obj2 = Simple()
+class B(A):
+    def bye(self):
+        print("グッバイ")
 
 
-def drum(beat="トコトコ"):
-    print(beat)
-
-
-def sax(phrase="ブーブー"):
-    print(phrase)
-
-
-obj1.play = drum
-obj2.play = sax
-
-# 追加したメンバーを削除
-obj1.play = None
-del obj2.play
-
-# 削除したので呼び出すことはできない！
-obj1.play()
-obj2.play()
+obj = B()
+obj.hello()
+obj.bye()
