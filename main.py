e89f59ef89a7f2ce3850687dd4d8c86c2214ec83
poketorena <poketorena@users.noreply.github.com>
2019-06-15 12:58:21 +0900

    colors_iterイテレータから要素を順に取り出す

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ colors = ["red", "blue", "green", "yellow"]
 colors_iter = iter(colors)
 type_dayo = type(colors_iter)
 
-print(type_dayo)
+print(next(colors_iter))
+print(next(colors_iter))
+print(next(colors_iter))
+print(next(colors_iter))
+print(next(colors_iter))
