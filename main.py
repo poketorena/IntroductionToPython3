7a2240c64e8189e53263ca22ce83fde623345031
poketorena <poketorena@users.noreply.github.com>
2019-06-15 17:17:51 +0900

    サブクラスからスーパークラスのコンストラクタを呼ぶ

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-class Greet():
-    def hello(self):
-        print("やあ！")
-
-    def bye(self):
-        print("さようなら")
+class Person():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
 
-class Greet2(Greet):
-    def hello(self, name=None):
-        if name:
-            print(name + "さんこんにちは！")
-        else:
-            super().hello()
+class Player(Person):
+    def __init__(self, name, age, number, position):
+        super().__init__(name, age)
+        self.number = number
+        self.position = position
 
 
-obj = Greet2()
-obj.hello("井上")
-obj.hello()
+player1 = Player("青木", 16, 10, "MF")
+
+print(player1.name)
+print(player1.age)
+print(player1.number)
+print(player1.position)
