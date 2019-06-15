4ba52b96388804aef10b2aa8ed9fbcea8ca1d89b
poketorena <poketorena@users.noreply.github.com>
2019-06-15 14:38:49 +0900

    car1、car2でdrive(10)を実行して、それぞれの総距離を確かめる

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-def main_gen(n):
-    yield "start"
-    yield from range(n, 0, -1)  # サブイテレータから値を作る
-    yield from "abc"  # サブイテレータから値を作る
-    yield from [10, 20, 30]  # サブイテレータから値を作る
-    yield from sub_gen()
-    yield "end"
+from car_class import Car
 
+car1 = Car.Car()
+car1.drive(15)
+car1.drive(20)
 
-# サブジェネレータ
-def sub_gen():
-    yield "X"
-    yield "Y"
-    yield "Z"
-
-
-gen = main_gen(3)
-print(list(gen))
+car2 = Car.Car()
+car2.drive(10)
+car1.drive(10)
