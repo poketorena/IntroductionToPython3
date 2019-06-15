2fca5476af34a80cb83bea893006b3051871fa3f
poketorena <poketorena@users.noreply.github.com>
2019-06-15 13:17:56 +0900

    genジェネレータから取り出した値でdo_something()を10回繰り返した結果

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-def menu_generator():
-    yield "ワイン"
-    yield "サラダ"
-    yield "スープ"
-    yield "ステーキ"
-    yield "アイスクリーム"
+def num_generator():
+    n = 0
+    while True:
+        num = n * n + 2 * n + 3
+        yield num
+        n += 1
 
 
-menu = menu_generator()
-print(type(menu))
+# 何かを扱う関数
+def do_something(num):
+    return (num % 2, num % 3)
 
-for item in menu:
-    print(item)
+
+# ジェネレータが返す値を使って処理を行う
+gen = num_generator()
+for i in range(1, 10):
+    num = next(gen)
+    result = do_something(num)
+    print(result)
