fd14b2ebfaf428782828c2460fdbfb3d17e06eed
poketorena <poketorena@users.noreply.github.com>
2019-06-15 16:07:57 +0900

    Simpleクラスに動的にクラス変数xを追加する

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from car_class import Car
+class Simple:
+    pass
 
-car1 = Car.Car()
-car2 = Car.Car("red")
-car3 = Car.Car("blue")
 
-print(car1.mynumber)
-print(car2.mynumber)
-print(car3.mynumber)
+Simple.x = 100
+print(Simple.x * 2)
