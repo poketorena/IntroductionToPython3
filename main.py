93ee149a0225f2b01ee05226252b91ae9571cd54
poketorena <poketorena@users.noreply.github.com>
2019-06-16 19:45:53 +0900

    Sinグラフを書く

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import matplotlib.pyplot as plt
+import math
 
-price = [200, 300, 400, 500, 600]
-count = [31, 29, 25, 28, 26]
-plt.plot(price, count, marker="o")
-plt.title("count - price")
-plt.xlabel("price")
-plt.ylabel("count")
-plt.grid(True)
+x = range(0, 360)
+y = [math.sin(math.radians(d)) for d in x]
+plt.plot(x, y)
 plt.show()
