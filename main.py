7997a05f244fbba7480b03bc208401fd6b9fe024
poketorena <poketorena@users.noreply.github.com>
2019-06-16 20:29:33 +0900

    第3の値をマーカーの大きさで表現する

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x, y = np.random.rand(100), np.random.rand(100)
-plt.scatter(x, y, marker="o", s=500, color="cyan", alpha=0.5, linewidths=2, edgecolors="b")
+v = np.random.rand(100) * 1000 + 50
+plt.scatter(x, y, marker="o", s=v, color="lightgreen", alpha=0.5, linewidths=2, edgecolors="green")
 plt.show()
