f25aa3851d654a158041c228b95cba0449f008db
poketorena <poketorena@users.noreply.github.com>
2019-06-16 20:17:20 +0900

    散布図を作る

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-labels = ["Green", "Red", "Yellow", "Blue", "Black", "White"]
-x_pos = range(0, 6)
-a = [34, 46, 54, 45, 56, 37]
-b = [17, 47, 55, 67, 38, 49]
-bar1 = plt.bar(x_pos, a, color="g")
-bar2 = plt.bar(x_pos, b, color="c", bottom=a)
-plt.xticks(x_pos, labels, rotation="vertical")
-plt.legend((bar1, bar2), ("man", "woman"), loc="upper right")
+x = np.random.rand(100)
+y = np.random.rand(100)
+
+plt.scatter(x, y)
 plt.show()
