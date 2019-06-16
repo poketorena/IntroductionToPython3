3f1dd6d3978a5708832504abe0a884332b9b7472
poketorena <poketorena@users.noreply.github.com>
2019-06-16 19:56:26 +0900

    凡例を表示する

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ y2 = [34, 56, 75, 91, 79]
 y3 = [25, 47, 68, 76, 73]
 y4 = [15, 40, 52, 64, 69]
 
-plt.plot(x, y1, marker="o", color="blue", linestyle="-")
-plt.plot(x, y2, marker="v", color="red", linestyle="--")
-plt.plot(x, y3, marker="^", color="green", linestyle="-.")
-plt.plot(x, y4, marker="d", color="m", linestyle=":")
+plt.plot(x, y1, marker="o", color="blue", linestyle="-", label="Y1")
+plt.plot(x, y2, marker="v", color="red", linestyle="--", label="Y2")
+plt.plot(x, y3, marker="^", color="green", linestyle="-.", label="Y3")
+plt.plot(x, y4, marker="d", color="m", linestyle=":", label="Y4")
+plt.legend(loc="upper left")  # 凡例を作る（位置は左上）
 plt.show()
