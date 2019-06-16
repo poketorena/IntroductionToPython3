1a68d2efa0304168e49674b78207f7b82fbe6f6a
poketorena <poketorena@users.noreply.github.com>
2019-06-16 19:48:46 +0900

    複数のグラフを重ね、保存する

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import math
 
 x = range(0, 360)
-y = [math.sin(math.radians(d)) for d in x]
-plt.plot(x, y)
+s = [math.sin(math.radians(d)) for d in x]
+c = [math.cos(math.radians(d)) for d in x]
+plt.plot(x, s)
+plt.plot(x, c)
+plt.savefig("sin_and_cos_graph.png")
 plt.show()
