25cc32d2126033c1f3e3dc4b4850d1d7f8770cbb
poketorena <poketorena@users.noreply.github.com>
2019-06-16 22:08:32 +0900

    第3の値をマーカーの色で表現するからやる！！！！

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-x, y = np.random.rand(100), np.random.rand(100)
-v = np.random.rand(100) * 1000 + 50
-plt.scatter(x, y, marker="o", s=v, color="lightgreen", alpha=0.5, linewidths=2, edgecolors="green")
-plt.show()
+# 第3の値をマーカーの色で表現するからやる！！！！
