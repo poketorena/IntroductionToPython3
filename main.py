0c5dd9da8ff64ded63a4aa48930e1035b43b8b87
poketorena <poketorena@users.noreply.github.com>
2019-06-16 18:43:49 +0900

    ダイアログを使ってファイルを読み込む

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-file = "./data/fox.txt"
-with open(file) as fileobj:
-    text = fileobj.read()
-    newtext = text.rstrip(".")
-    wordlist = newtext.split(" ")
-    print(wordlist)
+import tkinter as tk
+import tkinter.filedialog as fd
+
+# tkアプリをヘッドレスにする設定
+root = tk.Tk()
+root.withdraw()
+
+file = fd.askopenfilename(
+    title="ファイルを選んでください。",
+    filetypes=[("TEXT", ".txt"), ("TEXT", ".py"), ("HTML", ".html")]
+)
+
+if file:
+    with open(file, "r", encoding="utf_8")as fileobj:
+        text = fileobj.read()
+        print(text)
