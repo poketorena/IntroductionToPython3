6894ad1645cc9725c167aa94c2d3f63d45a7149f
poketorena <poketorena@users.noreply.github.com>
2019-06-16 18:16:26 +0900

    テキストファイルを読み込んで中身を表示する

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-from goods_property import Goods
-
-shoes = Goods("dream", 6800)
-print(shoes.name)
-shoes.name = "Dream 8"
-print(shoes.name)
-print(shoes.price)
-
-shoes2 = Goods("sky", 10000)
-print(shoes2.name)
-shoes2.name = "Sky 8"
-print(shoes2.name)
-print(shoes2.price)
-
-shoes.price = 7200
+file="./data/fox.txt"
+fileobj=open(file)
+text=fileobj.read()
+fileobj.close()
+print(text)
