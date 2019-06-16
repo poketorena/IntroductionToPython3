bb89f59bbc64212f73bdbc8c7645bc0272f2b355
poketorena <poketorena@users.noreply.github.com>
2019-06-16 20:11:06 +0900

    積み上げ棒グラフを作る

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 
-labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-x_pos = range(0, 10)
-v = [91, 45, 17, 88, 47, 87, 49, 56, 67, 77]
-plt.barh(x_pos, v, tick_label=labels)
+labels = ["Green", "Red", "Yellow", "Blue", "Black", "White"]
+x_pos = range(0, 6)
+a = [34, 46, 54, 45, 56, 37]
+b = [17, 47, 55, 67, 38, 49]
+bar1 = plt.bar(x_pos, a, color="g")
+bar2 = plt.bar(x_pos, b, color="c", bottom=a)
+plt.xticks(x_pos, labels, rotation="vertical")
+plt.legend((bar1, bar2), ("man", "woman"), loc="upper right")
 plt.show()
