666bccf323e6ed1a66a22ab353d94d2197f59cdd
poketorena <poketorena@users.noreply.github.com>
2019-06-16 19:38:28 +0900

    リストdataの数値をグラフ化する

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import tkinter as tk
-import tkinter.filedialog as fd
+import matplotlib.pyplot as plt
 
-# tkアプリをヘッドレスにする設定
-root = tk.Tk()
-root.withdraw()
-
-file = fd.askopenfilename(
-    title="ファイルを選んでください。",
-    filetypes=[("TEXT", ".txt"), ("TEXT", ".py"), ("HTML", ".html")]
-)
-
-if file:
-    with open(file, "r", encoding="utf_8")as fileobj:
-        text = fileobj.read()
-        print(text)
+data = [2, 2.3, 4.1, 2.4, 5.3, 3.2, 4.6]
+plt.plot(data)
+plt.show()
