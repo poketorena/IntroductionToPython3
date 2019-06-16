4a981af38138c34b32e2db458880e2fa62f68d91
poketorena <poketorena@users.noreply.github.com>
2019-06-16 20:19:44 +0900

    散布図のマーカーの種類と色を指定する

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.random.rand(100)
-y = np.random.rand(100)
+x1 = np.random.rand(100)
+y1 = np.random.rand(100)
 
-plt.scatter(x, y)
+x2 = np.random.rand(100)
+y2 = np.random.rand(100)
+
+plt.scatter(x1, y1, marker="+", color="green")
+plt.scatter(x2, y2, marker="^", color="red")
 plt.show()
