6991e52dcc69b47b6762537e991193a03f5a4736
poketorena <poketorena@users.noreply.github.com>
2019-06-17 22:56:18 +0900

    第3の値をマーカの色で表現する

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# 第3の値をマーカーの色で表現するからやる！！！！
+import matplotlib.pyplot as plt
+import numpy as np
+
+x, y = np.random.rand(100), np.random.rand(100)
+v = np.random.rand(100)
+plt.scatter(x, y, s=200, c=v, cmap="Blues", edgecolors="green")
+plt.colorbar()
+plt.grid(True)
+plt.show()
