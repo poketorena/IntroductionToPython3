139b919d7019d55df5b26187e1b8835ebebf1203
poketorena <poketorena@users.noreply.github.com>
2019-06-18 15:23:48 +0900

    グラフを上下に並べる

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x1, y1 = range(0, 5), [61, 45, 27, 88, 47]
-x2, y2 = range(0, 5), [17, 39, 46, 40, 27]
+x1, y1 = range(0, 7), [61, 45, 27, 88, 47, 56, 61]
+x2, y2 = range(0, 7), [17, 39, 46, 40, 27, 35, 41]
 
-labels = ["A", "B", "C", "D", "E"]
+labels = ["A", "B", "C", "D", "E", "F", "G"]
 fig = plt.figure()
 
 # 1行2列の左
-ax1 = fig.add_subplot(1, 2, 1)
+ax1 = fig.add_subplot(2, 1, 1, facecolor="cyan")
 ax1.bar(x1, y1, color="b", tick_label=labels)
-ax1.set_title("dog")
+ax1.set_title("snake")
 # 1行2列の右
-ax2 = fig.add_subplot(1, 2, 2)
+ax2 = fig.add_subplot(2, 1, 2, facecolor="cyan")
 ax2.bar(x2, y2, color="g", tick_label=labels)
-ax2.set_title("cat")
-
+ax2.set_title("fish")
+plt.tight_layout()
 plt.show()
