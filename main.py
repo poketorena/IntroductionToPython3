117f193975799fa72c20d1e357431d0ebc95d96f
poketorena <poketorena@users.noreply.github.com>
2019-06-18 19:24:17 +0900

    reshape()で作った配列bを変更すると元の配列aも変更される！

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import numpy as np
 
-a = np.array(list(range(10, 100, 10)))
-b = a.reshape(3, 3)
+a = np.array(list(range(10, 50, 10)))
+b = a.reshape(2, 2)
 print(a)
 print(b)
 
-print(b[0][0])
-print(b[0, 0])
+print(a is b)
 
-print(b[1][0])
-print(b[1, 0])
 
-print(b[1][1])
-print(b[1, 1])
-
-print(b[2][-1])
-print(b[2, -1])
+b[0, 0] = 99
+# 同じオブジェクトでないのにbの要素の変更がaに影響してしまう！
+print(b)
+print(a)
