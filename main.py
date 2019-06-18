6cdd6afaae67a3d670e47baa61bb9290472619da
poketorena <poketorena@users.noreply.github.com>
2019-06-18 19:42:49 +0900

    多次元配列のスライス

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import numpy as np
 
-a = np.array(list(range(0, 100, 10)))
+a = np.array(list(range(10, 100, 10))).reshape(3, 3)
 
-# 要素を逆順にした配列を取り出す
-print(a[::-1])
+print(a)
+print()
+# 多次元Numpy配列のスライスは[行のスライス指定, 列のスライス指定]
+# のように行と列のスライスの指定をカンマで区切って指定する！
+
+# 0～1行目、すべての列
+print(a[:2, ])
+print()
+
+# すべての行、1列目以降
+print(a[:, 1:])
+print()
+
+# 1行目以降、1列目以降
+print(a[1:, 1:])
