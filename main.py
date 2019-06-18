db069a0c9a77210104e8794512e1533f2ab5a576
poketorena <poketorena@users.noreply.github.com>
2019-06-18 16:00:19 +0900

    文字列の長さを指定してNumpy配列を作る

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import numpy as np
 
-a = np.array([1, 1.5, 2], dtype=int)
-print(a)
-
-b = np.array([1, 2, 3], dtype=float)
-print(b)
-
-c = np.array([1, 1.5, 1.5], dtype=complex)
-print(c)
-
-d = np.array(["1", 2, 3.8], dtype=int)
+d = np.array([1, 1.5, 2], dtype="<U")  # 文字列型で作る
 print(d)
+
+e = np.array([9.129, 10.5, 12.17], dtype="<U4")
+print(e)
