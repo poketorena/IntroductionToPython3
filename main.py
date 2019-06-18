42672d6c6435b1b0b8d8d18aec6efbbb9005067a
poketorena <poketorena@users.noreply.github.com>
2019-06-18 19:58:16 +0900

    偶数、奇数を分ける

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import numpy as np
 
 a = np.array([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3])
 print(a)
-# 5以上の値を抽出した配列を作る
-print(a[a >= 5])
+# 偶数
+print(a[a % 2 == 0])
+# 奇数
+print(a[a % 2 == 1])
