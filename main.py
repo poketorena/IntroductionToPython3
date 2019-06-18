5f3f2fb59139fc75267611758531d583f183080b
poketorena <poketorena@users.noreply.github.com>
2019-06-18 19:46:19 +0900

    スライスと同時に型変換する

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 import numpy as np
 
-a = np.array(list(range(10, 100, 10))).reshape(3, 3)
+a = np.array([2.1, 3.8, 2.5, 4.3, 5.1, 1.6]).reshape(3, 2)
 
 print(a)
-print()
-# 多次元Numpy配列のスライスは[行のスライス指定, 列のスライス指定]
-# のように行と列のスライスの指定をカンマで区切って指定する！
 
-# 0～1行目、すべての列
-print(a[:2, ])
-print()
-
-# すべての行、1列目以降
-print(a[:, 1:])
-print()
-
-# 1行目以降、1列目以降
-print(a[1:, 1:])
+a2 = a[:2, ].astype(int)
+print(a2)
