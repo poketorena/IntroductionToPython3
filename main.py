30a986cfa16d22f52ed11e4e0528710730e6f045
poketorena <poketorena@users.noreply.github.com>
2019-06-18 19:59:54 +0900

    多次元配列から5より大きな値を抽出する

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 
 a = np.array([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3])
 print(a)
-# 偶数
-print(a[a % 2 == 0])
-# 奇数
-print(a[a % 2 == 1])
+
+b = a.reshape(4, 4)
+print(b)
+print(b[b > 5])
