b3a1a294f060b6fbf3deefd6d06f074a1900001d
poketorena <poketorena@users.noreply.github.com>
2019-06-18 15:52:40 +0900

    型が混在しているNumpy配列は作ることができない

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import numpy as np
 
-a = np.array((1, 2, 3))
-print(type(a))
+a = np.array([1, 1.5, 2])
 print(a)
+
+b = np.array(["1", 1.5, 2])
+print(b)
+
+c = np.array([True, False, 1.5])
+print(c)
