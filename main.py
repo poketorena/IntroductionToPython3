9ddd779c1ca451f532776ec526742fae6c45a21b
poketorena <poketorena@users.noreply.github.com>
2019-06-18 15:48:16 +0900

    リストからNumpy配列を作る

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import matplotlib.pyplot as plt
 import numpy as np
 
-x1, y1 = range(0, 5), [61, 45, 27, 88, 47]
-x2, y2 = range(0, 5), [17, 39, 46, 40, 27]
+a = np.array([1, 2, 3])
+print(type(a))
+print(a)
 
-labels = ["A", "B", "C", "D", "E"]
-fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, sharey=True)
-
-ax1.bar(x1, y1, color="b", tick_label=labels)
-ax1.set_title("dog")
-ax2.bar(x2, y2, color="g", tick_label=labels)
-ax2.set_title("cat")
-plt.show()
