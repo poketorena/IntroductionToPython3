0d041773327a5afeec4fe97fe0d8edc6f5e6d4c7
poketorena <poketorena@users.noreply.github.com>
2019-06-18 16:07:07 +0900

    多次元配列を1次元配列にする

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-line1 = [10, 20, 30]
-line2 = [40, 50, 60]
-line3 = [70, 80, 90]
+data = [1, 2, 3, 4, 5, 6]
+a = np.array(data)
+print(a)
 
-a = np.array([line1, line2, line3])
+a = a.reshape(2, 3)
 print(a)
