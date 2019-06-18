2de7a7c51b4dc8ae817efa80c733e4b6b8cb1c85
poketorena <poketorena@users.noreply.github.com>
2019-06-18 19:13:18 +0900

    Numpy配列をリストに戻す

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import numpy as np
 
-a = np.array([0, 1, 2, 3, 4])
-b = a[:, np.newaxis]
+a = np.array([1, 2, 3, 4, 5])
+b = np.array([[0, 1], [2, 3], [4, 5]])
+a_list = a.tolist()
+b_list = b.tolist()
 print(a)
 print(b)
+print(a_list)
+print(b_list)
+
+print(type(a))
+print(type(b))
+print(type(a_list))
+print(type(b_list))
