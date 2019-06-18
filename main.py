5169105b7dd35688b34b470a43c50d692933ca34
poketorena <poketorena@users.noreply.github.com>
2019-06-18 15:56:43 +0900

    要素の型を指定してNumpy配列を作る

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import numpy as np
 
-a = np.array([1, 1.5, 2])
+a = np.array([1, 1.5, 2], dtype=int)
 print(a)
 
-b = np.array(["1", 1.5, 2])
+b = np.array([1, 2, 3], dtype=float)
 print(b)
 
-c = np.array([True, False, 1.5])
+c = np.array([1, 1.5, 1.5], dtype=complex)
 print(c)
+
+d = np.array(["1", 2, 3.8], dtype=int)
+print(d)
