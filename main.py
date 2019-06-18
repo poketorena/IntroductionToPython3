819f2d16a90f67e0374d28c774db05005babddf6
poketorena <poketorena@users.noreply.github.com>
2019-06-18 15:46:06 +0900

    Y軸を共有したサブプロットにグラフを表示する

--- main.py
+++ main.py
@@ -5,16 +5,10 @@ x1, y1 = range(0, 5), [61, 45, 27, 88, 47]
 x2, y2 = range(0, 5), [17, 39, 46, 40, 27]
 
 labels = ["A", "B", "C", "D", "E"]
-fig = plt.figure()
+fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, sharey=True)
 
-# 1行2列の左
-ax1 = fig.add_subplot(1, 2, 1)
 ax1.bar(x1, y1, color="b", tick_label=labels)
 ax1.set_title("dog")
-ymin, ymax = plt.ylim()  # 現在のY軸のレンジを取得する
-# 1行2列の右
-ax2 = fig.add_subplot(1, 2, 2)
 ax2.bar(x2, y2, color="g", tick_label=labels)
 ax2.set_title("cat")
-plt.ylim(ymin, ymax)
 plt.show()
