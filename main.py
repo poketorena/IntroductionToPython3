0412e6f458ecf12debda3014ef21ccc00ad73300
poketorena <poketorena@users.noreply.github.com>
2019-06-18 18:21:52 +0900

    配列に要素を挿入する

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import numpy as np
 
-a = np.array([1, 2, 3, 4, 5, 6]).reshape(2, 3)
+a = np.array([0, 1, 2])
 print(a)
-b = np.append(a, [[7, 8, 9]], axis=0)
+b = np.insert(a, 1, 99)
 print(b)
-c = np.append(a, [[7], [8]], axis=1)
-print(c)
