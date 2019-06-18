0464d5fd612a927dc94fd051d5e1325949d5b02e
poketorena <poketorena@users.noreply.github.com>
2019-06-18 19:20:17 +0900

    3行3列のNumpy配列の要素にアクセスする

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import numpy as np
 
-a = np.array(list(range(10, 60, 10)))
-
-a[2] = 99
+a = np.array(list(range(10, 100, 10)))
+b = a.reshape(3, 3)
 print(a)
-print(a[0])
-print(a[1])
-print(a[-1])
+print(b)
+
+print(b[0][0])
+print(b[0, 0])
+
+print(b[1][0])
+print(b[1, 0])
+
+print(b[1][1])
+print(b[1, 1])
+
+print(b[2][-1])
+print(b[2, -1])
