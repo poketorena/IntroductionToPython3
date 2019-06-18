f64eae28b120270b66eab04033c0e3d1bb77a138
poketorena <poketorena@users.noreply.github.com>
2019-06-18 19:30:30 +0900

    変更を伝搬させたくないときはコピーして使おう（自動で浅いコピーか深いコピーかを判別してくれる！）
    どうも，n次元配列に直せそうなものに対しては深いコピーを実施するらしい

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import numpy as np
 
 a = np.array(list(range(10, 50, 10)))
-b = a.reshape(2, 2)
+# 浅いコピーと深いコピーのどちらを実行するか自動で判断される
+b = a.copy()
+b = b.reshape(2, 2)
+
 print(a)
 print(b)
 
 print(a is b)
-
 
 b[0, 0] = 99
 # 同じオブジェクトでないのにbの要素の変更がaに影響してしまう！
