5024a567b51e8130b22fae23c2715b6f7bce884b
poketorena <poketorena@users.noreply.github.com>
2019-06-18 16:21:02 +0900

    2x3Numpy配列に行、列を追加する

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-a = np.array([0, 1, 2])
-b = np.append(a, [3, 4, 5])
-c = np.append(a, (6, 7, 8))
+a = np.array([1, 2, 3, 4, 5, 6]).reshape(2, 3)
 print(a)
+b = np.append(a, [[7, 8, 9]], axis=0)
 print(b)
+c = np.append(a, [[7], [8]], axis=1)
 print(c)
