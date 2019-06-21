2ecdfc0ef406312bc4fb2991ecff9a1adfbffd15
poketorena <poketorena@users.noreply.github.com>
2019-06-21 12:37:16 +0900

    配列の値でSinグラフを描く

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
-data = [0.0, 0.28, 0.57, 0.85, 1.14, 1.42, 1.71, 1.99, 2.28, 2.57, 2.85, 3.14]
-# print(math.sin(data))
-print(np.sin(data))
+x = np.linspace(-np.pi, np.pi, 180)
+y = np.sin(x)
+plt.plot(x, y)
+plt.show()
