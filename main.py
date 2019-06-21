3fec6b47259c2c95eedd5c7fc299bef36c7b1e84
poketorena <poketorena@users.noreply.github.com>
2019-06-21 14:32:41 +0900

    要素をシャッフルする

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-np.random.seed(10)
-print(np.random.randn(3))
-
-print(np.random.seed(10))
-print(np.random.randn(3))
+data = np.arange(9).reshape(3, 3)
+np.random.shuffle(data)
+print(data)
