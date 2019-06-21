ebedc3e2af9620e93bfed305a5bf1a87a840c5aa
poketorena <poketorena@users.noreply.github.com>
2019-06-21 12:11:46 +0900

    5より大きい要素が入っている位置を調べる

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import numpy as np
 
-sigma = 3.5  # 分散
-mu = 65  # 平均
-# 点数のサンプルデータ（正規分布の乱数で作成する）
-data = sigma * np.random.randn(200) + mu  # 点数が200個入った配列
-x = float(input("得点は？："))  # キーボードから得点を入力する
-t_score = 10 * (x - data.mean()) / data.std() + 50  # 偏差値
-print("平均点：", round(data.mean(), 1))
-print("標準偏差：", round(data.std(), 1))  # 標準偏差（小数点以下第1位まで）
-print("偏差値：", round(t_score, 1))
+a = np.array([4, 6, 3, 1, 7, 3])
+b = a > 5
+print(b)
