9d483bb37f1c2575c73ddceb0540cb163bcb14c2
poketorena <poketorena@users.noreply.github.com>
2019-06-21 12:13:49 +0900

    同じ行列数の配列の足し算

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import numpy as np
 
-a = np.array([4, 6, 3, 1, 7, 3])
-b = a > 5
+a = np.array([1, 2, 3, 4]).reshape(2, 2)
+print(a)
+b = np.array([10, 20, 30, 40]).reshape(2, 2)
 print(b)
+c = a + b
+print(c)
