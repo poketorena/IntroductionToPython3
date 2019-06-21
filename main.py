12626ab2e845003375b0cf4ccb6f21703f6d472c
poketorena <poketorena@users.noreply.github.com>
2019-06-21 12:03:38 +0900

    Numpy配列の合計値、最大値、最小値、平均値

--- main.py
+++ main.py
@@ -9,3 +9,48 @@ print(a.sum(0))  # 各列の合計
 
 print(a.sum(axis=1))  # 各行の合計
 print(a.sum(1))  # 各行の合計
+
+print("最大値")
+
+print(a.max())  # 全体の最大値
+print(np.max(a))  # 全体の最大値
+
+print(a.max(axis=0))  # 各列の最大値
+print(a.max(0))  # 各列の最大値
+print(np.max(a, axis=0))  # 各列の最大値
+print(np.max(a, 0))  # 各列の最大値
+
+print(a.max(axis=1))  # 各行の最大値
+print(a.max(1))  # 各行の最大値
+print(np.max(a, axis=1))  # 各行の最大値
+print(np.max(a, 1))  # 各行の最大値
+
+print("最小値")
+
+print(a.min())  # 全体の最小値
+print(np.min(a))  # 全体の最小値
+
+print(a.min(axis=0))  # 各列の最小値
+print(a.min(0))  # 各列の最小値
+print(np.min(a, axis=0))  # 各列の最小値
+print(np.min(a, 0))  # 各列の最小値
+
+print(a.min(axis=1))  # 各行の最小値
+print(a.min(1))  # 各行の最小値
+print(np.min(a, axis=1))  # 各行の最小値
+print(np.min(a, 1))  # 各行の最小値
+
+print("平均値")
+
+print(a.mean())  # 全体の平均値
+print(np.mean(a))  # 全体の平均値
+
+print(a.mean(axis=0))  # 各列の平均値
+print(a.mean(0))  # 各列の平均値
+print(np.mean(a, axis=0))  # 各列の平均値
+print(np.mean(a, 0))  # 各列の平均値
+
+print(a.mean(axis=1))  # 各行の平均値
+print(a.mean(1))  # 各行の平均値
+print(np.mean(a, axis=1))  # 各行の平均値
+print(np.mean(a, 1))  # 各行の平均値
