0114a18be731080ddd7ca62f86d090929ed4f162
poketorena <poketorena@users.noreply.github.com>
2019-06-21 11:52:02 +0900

    ベクトルaの長さ

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import numpy as np
 
-p0=np.array((1,1))
-p1=np.array((6,4))
-a=p1-p0
+p0 = np.array((1, 1))
+p1 = np.array((6, 4))
+a = p1 - p0
 print(type(a))
-print(a)
+print("a", a)
+
+a_norm = np.linalg.norm(a)
+print(a_norm)
+
+a2 = a * 2
+print("a2", a2)
+a2_norm = np.linalg.norm(a2)
+print(a2_norm)
