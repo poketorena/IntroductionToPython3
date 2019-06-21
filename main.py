0f68df6713918466064d94de6d34013db946741f
poketorena <poketorena@users.noreply.github.com>
2019-06-21 11:36:00 +0900

    Numpy配列を降順にソート（スライスを使う）

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import numpy as np
 
 a = np.array([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3])
-a = a.reshape(4, 4)
 print(a)
-print()
 
-a[a % 2 == 0] = 0  # 偶数を0に置き換える
-print(a)
-print()
+# クラスメソッドだと要素をソートした新しいNumpy配列を返す
+a_descend = np.sort(a)[::-1]
 
-a[a % 2 == 1] = 1  # 奇数を1に置き換える
-print(a)
-print()
+print("a", a)
+print("a_descend", a_descend)
