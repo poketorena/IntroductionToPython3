ce7c9153a6a0069315364205522042b212311fe1
poketorena <poketorena@users.noreply.github.com>
2019-06-21 14:29:07 +0900

    ポアソン分布をヒストグラムで表示する

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-result = np.random.poisson(lam=10, size=(10))
-print(result)
+data = np.random.poisson(lam=50, size=1000)
+count, bins_edges, patches = plt.hist(data, bins=100)  # ヒストグラム
+plt.grid()
+plt.show()
