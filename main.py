f891c21fcd6e7f8ce39f3a7c605b08b4b540e233
poketorena <poketorena@users.noreply.github.com>
2019-06-21 14:02:10 +0900

    初期化していない行列を作る

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import numpy as np
 
-result = np.linspace(0, 120, 16)
-print(result)
-
-# num（分割数）を省略すると50分割になる
-result = np.linspace(0, 120)
-print(result)
+data = np.empty((3, 2), dtype=int)
+print(data)
