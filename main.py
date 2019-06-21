2aaa5767db1aae9b7f2d88f7e63f2801d7a199c2
poketorena <poketorena@users.noreply.github.com>
2019-06-21 12:24:23 +0900

    行列の内積

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import numpy as np
 
-a = np.array([1, 2, 3, 4, 5, 6]).reshape(2, 3)
-print(a)
-b = np.array([10, 20]).reshape(2, 1)
-print(b)
-c = a + b
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6], [7, 8]])
+c = np.dot(a, b)
 print(c)
