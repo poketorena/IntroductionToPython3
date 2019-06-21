70fc5c86b57a52114ef2093de0e3b0892d7bfe8d
poketorena <poketorena@users.noreply.github.com>
2019-06-21 12:17:41 +0900

    ベクトルの足し算と引き算

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import numpy as np
 
-a = np.array([1, 2, 3, 4]).reshape(2, 2)
-print(a)
-b = np.array([10, 20, 30, 40]).reshape(2, 2)
-print(b)
+a = np.array([5, 3])
+b = np.array([4, -2])
 c = a + b
+d = a - b
+print(a)
+print(b)
 print(c)
-d = a * b
-e = a / b
 print(d)
-print(e)
