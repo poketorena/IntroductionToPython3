2a0457345ac411fa16e0425c85037f6c15ea5a70
poketorena <poketorena@users.noreply.github.com>
2019-06-21 11:41:34 +0900

    Numpy配列の全ての要素に詩側縁座員を行う（ブロードキャスト）

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import numpy as np
 
-a = np.array([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3])
+a = np.arange(10, 50, 10).reshape(2, 2)
 print(a)
-
-# クラスメソッドだと要素をソートした新しいNumpy配列を返す
-a_descend = np.sort(a)[::-1]
-
+b = a + 5
+c = a - 5
+d = a * 2
+e = a / 2
+f = a // 2
 print("a", a)
-print("a_descend", a_descend)
+print("b", b)
+print("c", c)
+print("d", d)
+print("e", e)
+print("f", f)
