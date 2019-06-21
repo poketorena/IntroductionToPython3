9760b03aa002a6b35284dd9584c9d518cc3cf3e8
poketorena <poketorena@users.noreply.github.com>
2019-06-21 13:56:27 +0900

    行列A、Bを比較して要素が同じ数、要素が異なる数を数える

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import numpy as np
 
-arr = np.arange(10, step=0.5)
-print(arr)
+a = np.array([1, 2, 3, 4, 5, 6]).reshape(2, 3)
+b = np.array([1, 2, 9, 4, 8, 6]).reshape(2, 3)
+
+print(a)
+print(b)
+
+c = (a == b)
+print(c)
+
+# 行列A、Bを比較して要素が同じ数、要素が異なる数を数える
+print(np.sum(a == b))
+print(np.sum(a != b))
