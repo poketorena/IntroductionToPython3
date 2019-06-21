abb44a8858c149cf72c1876934397dca0cec5727
poketorena <poketorena@users.noreply.github.com>
2019-06-21 13:59:58 +0900

    範囲を等分割したNumpy配列を作る

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import numpy as np
 
-a = np.array([1, 2, 3, 4, 5, 6]).reshape(2, 3)
-b = np.array([1, 2, 9, 4, 8, 6]).reshape(2, 3)
+result = np.linspace(0, 120, 16)
+print(result)
 
-print(a)
-print(b)
-
-c = (a == b)
-print(c)
-
-# 行列A、Bを比較して要素が同じ数、要素が異なる数を数える
-print(np.sum(a == b))
-print(np.sum(a != b))
+# num（分割数）を省略すると50分割になる
+result = np.linspace(0, 120)
+print(result)
