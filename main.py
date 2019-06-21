9ad721d39e398edc7e63f717ef19efaf4f68d39b
poketorena <poketorena@users.noreply.github.com>
2019-06-21 12:21:21 +0900

    列数が足りない配列を返す

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-a = np.array([1, 2, 3, 4]).reshape(2, 2)
+a = np.array([1, 2, 3, 4, 5, 6]).reshape(2, 3)
 print(a)
-b = np.array([100, 200])
+b = np.array([10, 20]).reshape(2, 1)
 print(b)
 c = a + b
 print(c)
