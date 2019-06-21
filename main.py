2aca73d3c2a293a2a1b5b0135936b5dc0b7221f0
poketorena <poketorena@users.noreply.github.com>
2019-06-21 12:29:20 +0900

    dot()の結果を検算する

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ import numpy as np
 
 f = np.array([8.66, 5.0])
 s = np.array([20, 0])
-w = np.dot(f, s)
+
+f = np.linalg.norm(f)
+s = np.linalg.norm(s)
+rad = np.radians(30)  # 30度をラジアンに換算
+w = f * s * np.cos(rad)
 print(w)
