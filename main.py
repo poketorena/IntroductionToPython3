8b7f1a2e6a0a85ade772a55e5d2c38b20b5ce5bf
poketorena <poketorena@users.noreply.github.com>
2019-06-21 11:20:36 +0900

    偶数は0、奇数は1に置き換える

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# 条件にある要素を変更するから再開する！
+import numpy as np
+
+a = np.array([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3])
+a = a.reshape(4, 4)
+print(a)
+print()
+
+a[a % 2 == 0] = 0  # 偶数を0に置き換える
+print(a)
+print()
+
+a[a % 2 == 1] = 1  # 奇数を1に置き換える
+print(a)
+print()
