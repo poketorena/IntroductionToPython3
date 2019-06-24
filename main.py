40e9e1d0d2c256c48c65ccc7f6bdc71fc80ee0b1
poketorena <poketorena@users.noreply.github.com>
2019-06-24 12:16:08 +0900

    学習機をLogisticRegressionに取り換えて試してみる

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from sklearn import datasets
-from sklearn import svm
+from sklearn import linear_model
 from sklearn.model_selection import ShuffleSplit
 
 iris = datasets.load_iris()  # アヤメのデータセットを読み込む
@@ -13,7 +13,7 @@ train_index, test_index = next(ss.split(x))  # 分割するインデックス番
 x_train, y_train = x[train_index], y[train_index]  # 訓練データ
 x_test, y_test = x[test_index], y[test_index]  # テストデータ
 
-clf = svm.SVC()  # モデルを作る
+clf = linear_model.LogisticRegression()  # 学習機のアルゴリズムを変えてみる
 clf.fit(x_train, y_train)  # 学習する
 
 print(clf.score(x_test, y_test))
